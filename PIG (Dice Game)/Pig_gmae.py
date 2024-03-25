8a2a46e5bde5df2394514af3a55008e7d9dd227f
syex0x <s.r.mahamid@gmail.com>
2024-03-25 23:13:47 +0500

    features

--- PIG (Dice Game)/Pig_gmae.py
+++ PIG (Dice Game)/Pig_gmae.py
@@ -1,4 +1,5 @@
 import random
+import os
 
 def dice_roll(score):
     while True:
@@ -14,28 +15,51 @@ def dice_roll(score):
             roll=random.randint(1,6)
             print(roll)
             score+=roll
-        else:
+        elif opt=="2":
             return score
+        else:
+            print("Invalid Input")
+
     
     
+def splayer():
+    pscore=0
+    cscore=0
+    while True:
+        
+        dice_roll(pscore)
+
+
+
 
 
 def main():
     while True:
-        print(dice_roll(2))
+        os.system("cls")
         print("Press 1 to play vs Comp")
         print("Press 2 for PVP")
         print("Press E to exit")
+        
         opt=input("Enter Here:").lower()
         if opt=="1":
-            pass
+            
+            print("\nRULES")
+            print("You can roll the Dice till you get a one")
+            print("You can add all the score except 1")
+            print("if you save the score before getting a one")
+            print("the score will be added otherwise")
+            print("all the score will be lost\n")
+            os.system("pause")
+
         elif opt=="2":
             pass
         elif opt=="e":
+
             print("GoodBye")
-            exit
+            break
         else:
             print("Invalid Input")
+
 
     
 
