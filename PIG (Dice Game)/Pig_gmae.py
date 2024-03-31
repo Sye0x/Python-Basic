cd96dd15d4999e84fa4dc5b39af85c91b62ce7df
syex0x <s.r.mahamid@gmail.com>
2024-03-31 06:02:17 +0500

    update

--- PIG (Dice Game)/Pig_gmae.py
+++ PIG (Dice Game)/Pig_gmae.py
@@ -2,25 +2,41 @@ import random
 import os
 
 def dice_roll(score):
+    sc=0
     while True:
-    
+        
         print("Press 1 to roll the dice")
         print("Press 2 to Stop")
         opt=input("Enter Here:")
-        if score >=50:
-            return score
-
+        if score+sc >=50:
+            return sc
+        
 
         if opt=="1":
             roll=random.randint(1,6)
+            if roll==1:
+                return 0
             print(roll)
-            score+=roll
+            sc+=roll
         elif opt=="2":
-            return score
+            return sc
         else:
             print("Invalid Input")
 
-    
+def cdice_roll(score,n):
+    i=0
+    sc=0
+    while i<n:
+        i+=1
+        if score+sc >=50:
+            return sc
+
+        roll=random.randint(1,6)
+        if roll==1:
+                return 0
+        
+        sc+=roll
+    return sc
     
 def splayer():
     pscore=0
@@ -28,10 +44,21 @@ def splayer():
     while True:    
         os.system("cls")
         pscore+= dice_roll(pscore)
-        print(pscore)
+        print("Player-Score:"+str(pscore))
         os.system("pause")
-        break
+        roll=random.randint(1,6)
+        cscore+=cdice_roll(cscore,roll)
+        print("Comp-Score:"+str(cscore))
+        os.system("pause")
+        if pscore>=50:
+            print("Player Wins")
+            break
+        elif cscore>=50:
+            print("Comp Wins")
+            break
+
         
+         
 
 
 
@@ -56,8 +83,6 @@ def main():
             os.system("pause")
             splayer()
 
-
-
         elif opt=="2":
             pass
         elif opt=="e":
@@ -66,7 +91,5 @@ def main():
         else:
             print("Invalid Input")
 
-
-    
 
 main()
