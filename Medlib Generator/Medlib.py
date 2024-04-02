51b78b08f1989503b052e4517af5980b74fc089d
syex0x <s.r.mahamid@gmail.com>
2024-04-02 22:55:26 +0500

    Medlib Gen Complete

--- Medlib Generator/Medlib.py
+++ Medlib Generator/Medlib.py
@@ -1,6 +1,40 @@
-def readfile():
-    pass
-def getwords():
-    pass
+def readstory():
+    with open('story.txt','r') as f:
+        story = f.read()
+
+        words = set()
+        targets = "["
+        targete = "]"
+        start_of_word = -1
+        for i, char in enumerate(story):
+            if char == targets:
+                start_of_word = i
+            
+            if targete == char and start_of_word != -1:
+                word = story[start_of_word:i+1]
+                words.add(word)
+
+        return words
+
+
+def getwords(words):
+    wordlist = {}
+    for word in words:
+        wordlist[word] = input("Enter a word for " + word + ": ")
+    return wordlist
+
+
 def main():
-    pass
+    with open('story.txt','r') as f:
+        story = f.read()
+
+    word = readstory()
+    wordlist = getwords(word)
+
+    for old_word, new_word in wordlist.items():
+        story = story.replace(old_word, new_word)
+
+    print(story)
+
+
+main()
