a729a72f4dd94eab60e9c34a241fbee2230345b5
syex0x <s.r.mahamid@gmail.com>
2024-04-15 20:57:35 +0500

    Starting

--- Timed_math_quiz/math_quiz.py
+++ Timed_math_quiz/math_quiz.py
@@ -1,3 +1,32 @@
-def main():
+import random
+
+print("Press 1 for Level 1")
+print("Press 2 for Level 2")
+print("Press 3 for Level 3")
+x=int(input("Enter Here:"))
+if x==1:
+    i=0
+    while i<10:
+        i=i+1
+        operands=["+","-","*","/"]
+        pick=random.randint(0,3)
+        opt=operands[pick]
+        num1=random.randint(0,22)
+        num2=random.randint(0,22)
+        print(str(num1)+" "+str(opt)+" "+str(num2))
+        
+    
+elif x==2:
     pass
-main()
+elif x==3:
+    pass
+else:
+    pass
+
+
+
+
+
+
+
+  
